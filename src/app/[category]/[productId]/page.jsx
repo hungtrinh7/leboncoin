b34c6a3b7c4b37c1f0d7c7c766d043ca28e097d5
Olivier <olivierhung.trinh@gmail.com>
2024-02-23 10:30:34 +0100

    open and close modal done

--- src/app/[category]/[productId]/page.jsx
+++ src/app/[category]/[productId]/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import {
   Calendar,
+  Euro,
   Heart,
   MapPin,
+  MessageSquareText,
   MessagesSquare,
   MoveRight,
   Share2,
@@ -23,14 +25,127 @@ export default function Page({ params }) {
   const product = PRODUCTS.find(
     (product) => product.id === Number(params.productId)
   );
-  const [showModal, setShowModal] = useState(false);
+  const [showModalHowItWorks, setShowModalHowItWorks] = useState(false);
+  const [showModalMore, setShowModalMore] = useState(false);
 
-  const handleOpenModal = () => {
-    setShowModal(true);
+  const handleOpenModalHowItWorks = () => {
+    setShowModalHowItWorks(true);
+  };
+  const handleOpenModalMore = () => {
+    setShowModalMore(true);
   };
 
   return (
     <>
+      {showModalHowItWorks && (
+        <Modal onShowModal={setShowModalHowItWorks}>
+          <p className="text-[#094171] font-bold text-base mt-4">
+            Les étapes de la remise en main propre avec paiement sécurisé
+          </p>
+          <div className="flex items-center mt-2">
+            <div className="p-2 mr-2 h-8 w-8 bg-[#E6F1FD] rounded-full">
+              <Smile size={16} color="black" fill="#E6F1FD" />
+            </div>
+            <span>
+              En réservant l’article, le vendeur vous confirme la disponibilité
+              de l’article
+            </span>
+          </div>
+          <div className="flex items-center mt-2">
+            <div className="p-2 mr-2 h-8 w-8 bg-[#E6F1FD] rounded-full">
+              <Calendar size={16} color="black" fill="#E6F1FD" />
+            </div>
+            <span>
+              Vous vous organisez avec le vendeur pour définir le lieu et la
+              date de votre rendez-vous
+            </span>
+          </div>
+          <div className="flex items-center mt-2">
+            <div className="p-2 mr-2 h-8 w-8 bg-[#E6F1FD] rounded-full">
+              <ShieldCheck size={16} color="black" fill="#E6F1FD" />
+            </div>
+            <span>
+              Pensez à prendre votre téléphone portable pour déclencher le
+              paiement depuis votre messagerie leboncoin pendant le rendez-vous
+            </span>
+          </div>
+        </Modal>
+      )}
+      {showModalMore && (
+        <Modal onShowModal={setShowModalMore}>
+          <Image
+            src={"/protection-leboncoin.svg"}
+            width={592}
+            height={143}
+            alt="protection leboncoin"
+          />
+          <div className="mt-6">
+            <Image
+              src={"/protection-lbc.svg"}
+              width={236}
+              height={22}
+              alt="protection leboncoin"
+            />
+          </div>
+          <ul className="mt-8">
+            <li className="flex gap-2 mb-lg">
+              <div>
+                <ShieldCheck size={24} color="#B84A14" />
+              </div>
+              <div>
+                <h2 className="text-subhead">
+                  Nous sécurisons votre argent jusqu’à réception
+                </h2>
+                <p className="text-sm">
+                  En cas de remise en main propre, votre argent est conservé sur
+                  un compte séquestre jusqu’à ce que vous confirmiez avoir
+                  récupéré le Bien. En cas de livraison, vous disposez d’un
+                  délai de 10 jours après la confirmation d’envoi du colis pour
+                  en confirmer la bonne réception. Le vendeur est payé
+                  uniquement lorsque vous confirmez la conformité du Bien et que
+                  vous n’avez pas signalé de problème auprès de nos services,
+                  dans un délai de 3 jours.
+                </p>
+              </div>
+            </li>
+            <li className="flex gap-2 mb-lg mt-4">
+              <div>
+                <MessageSquareText size={24} color="#B84A14" />
+              </div>
+              <div>
+                <h2 className="text-subhead">Nous sommes à votre écoute</h2>
+                <p className="text-sm">
+                  Notre Service Client est là pour vous aider, à compter du 6ème
+                  jour après la confirmation d’envoi du colis dans le cadre
+                  d’une livraison, et jusqu’au 3ème jour à compter de la
+                  confirmation de réception du colis par vos soins. Si vous ne
+                  récupérez pas votre Bien ou qu’il est indisponible, demandez
+                  l’assistance de nos services afin d’obtenir votre éventuel
+                  remboursement dans les plus brefs délais.{" "}
+                  <span className="font-bold">
+                    Pour toute autre question, vous pouvez contacter nos
+                    services. <span className="underline">En savoir plus.</span>
+                  </span>
+                </p>
+              </div>
+            </li>
+            <li className="flex gap-2 mb-lg mt-4">
+              <div>
+                <Euro size={24} color="#B84A14" />
+              </div>
+              <div>
+                <h2 className="text-subhead">Ce que vous payez</h2>
+                <p className="text-sm">
+                  Le montant total se compose du prix du Bien convenu avec votre
+                  vendeur, d’éventuels frais de livraison et de frais de service
+                  (4 % du prix d’achat effectif du Bien avec un minimum de 0,99
+                  €).
+                </p>
+              </div>
+            </li>
+          </ul>
+        </Modal>
+      )}
       <Breadcrumb />
       <div className="flex max-w-5xl mx-auto mt-6 mb-4">
         <div className="w-3/5">
@@ -96,7 +211,9 @@ export default function Page({ params }) {
               href={"#"}
               className="flex items-center text-base text-[#094171] font-bold"
             >
-              <span className="mr-2">En savoir plus</span>{" "}
+              <span className="mr-2" onClick={handleOpenModalMore}>
+                En savoir plus
+              </span>{" "}
               <MoveRight size={16} />
             </Link>
           </div>
@@ -143,45 +260,10 @@ export default function Page({ params }) {
             </div>
             <p
               className="mt-4 text-base underline font-bold cursor-pointer"
-              onClick={handleOpenModal}
+              onClick={handleOpenModalHowItWorks}
             >
               Comment ça marche ?
             </p>
-            {showModal && (
-              <Modal>
-                <p className="text-[#094171] font-bold text-base mt-4">
-                  Les étapes de la remise en main propre avec paiement sécurisé
-                </p>
-                <div className="flex items-center mt-2">
-                  <div className="p-2 mr-2 h-8 w-8 bg-[#E6F1FD] rounded-full">
-                    <Smile size={16} color="black" fill="#E6F1FD" />
-                  </div>
-                  <span>
-                    En réservant l’article, le vendeur vous confirme la
-                    disponibilité de l’article
-                  </span>
-                </div>
-                <div className="flex items-center mt-2">
-                  <div className="p-2 mr-2 h-8 w-8 bg-[#E6F1FD] rounded-full">
-                    <Calendar size={16} color="black" fill="#E6F1FD" />
-                  </div>
-                  <span>
-                    Vous vous organisez avec le vendeur pour définir le lieu et
-                    la date de votre rendez-vous
-                  </span>
-                </div>
-                <div className="flex items-center mt-2">
-                  <div className="p-2 mr-2 h-8 w-8 bg-[#E6F1FD] rounded-full">
-                    <ShieldCheck size={16} color="black" fill="#E6F1FD" />
-                  </div>
-                  <span>
-                    Pensez à prendre votre téléphone portable pour déclencher le
-                    paiement depuis votre messagerie leboncoin pendant le
-                    rendez-vous
-                  </span>
-                </div>
-              </Modal>
-            )}
             <hr className="my-lg block border-t-sm border-outline my-6 opacity-20" />
             <div className="flex items-center">
               <MapPin size={16} />
--- src/app/components/UI/Modal.jsx
+++ src/app/components/UI/Modal.jsx
@@ -2,18 +2,20 @@ import { X } from "lucide-react";
 import React from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const handleClose = () => {};
+const Modal = ({ children, onShowModal }) => {
+  const handleClose = () => {
+    onShowModal(false);
+  };
 
   return createPortal(
-    <div className="fixed left-0 top-0 bg-black bg-opacity-50 w-screen h-screen flex justify-center items-center z-20">
-      <div className="relative bg-white rounded-2xl shadow-md p-8 w-[672px] mx-auto h-60">
+    <dialog className="fixed left-0 top-0 bg-black bg-opacity-50 w-screen h-screen flex justify-center items-center z-20">
+      <div className="relative bg-white rounded-2xl shadow-md p-8 w-[672px] mx-auto h-auto">
         <button className="absolute top-2 right-6 p-2 hover:bg-[#627C93] hover:bg-opacity-10 rounded-2xl">
           <X size={24} color="#627C93" onClick={handleClose} />
         </button>
         {children}
       </div>
-    </div>,
+    </dialog>,
     document.getElementById("modal")
   );
 };
